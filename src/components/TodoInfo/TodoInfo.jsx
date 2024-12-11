c29d125a58ca7e39cf2b4b5e745200ebd2c3cea6
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2024-12-11 17:11:36 +0100

    problem with code

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,12 @@
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo, user }) => (
+  <article
+    className={`TodoInfo ${todo.completed === true ? 'TodoInfo--completed' : ''}`}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {user && <UserInfo user={user} />}
+  </article>
+);
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,14 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos = [], users = [] }) => (
+  <section className="TodoList">
+    {todos.map(todo => {
+      const user = Array.isArray(users)
+        ? users.find(u => u.id === todo.userId) // Потрібно порівнювати todo.userId
+        : null;
+
+      return <TodoInfo key={todo.id} todo={todo} user={user} />;
+    })}
+  </section>
+);
